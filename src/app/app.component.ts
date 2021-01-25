d3b959ea7fdf2902b7279068f86a8a8017709a70
Matias Troncoso C <mtroncoc@everis.com>
2021-01-25 09:46:52 -0300

    v.1 stable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'showcase-test';
+  data = {
+    documentCollection: [
+      {
+        created: new Date(),
+        createdBy: 'Demo user',
+        ezshareId: '0123-4567-89',
+        id: 45,
+        name: 'example.pdf',
+        type: 'MainDocument',
+        version: '0.1',
+      },
+    ],
+    historyDocumentCollection: [],
+  };
+  settings = {
+    dateFormat: 'dd MMM yyyy',
+    disclosureResponse: '',
+    documentHistoryTypesCollection: ['MainDocument'],
+    documentRestriction: {
+      allowedExtensions: ['.pdf'],
+      maxFileSize: 4194304,
+    },
+    expanded: true,
+    limit: 1,
+    readonly: false,
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { DocumentViewerModule } from 'idb-business-cross-ui-components';
+import { TimelineModule } from 'idb-business-cross-ui-components';
+// import { CommentsModule } from 'idb-business-cross-ui-components';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    DocumentViewerModule,
+    TimelineModule,
+    // CommentsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
